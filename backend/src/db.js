d91dd5e596ece127b79113c268ac0f5b3a2910b4
Matt Nierzwicki <matt.nierzwicki@gmail.com>
2018-11-18 00:41:12 -0500

    Add User type to graphQL schema

---
+++ backend/src/db.js
@@ -0,0 +1,12 @@
+// Connect to remote prisma db and allow querying via JS
+
+const { Prisma } = require("prisma-binding");
+
+const db = new Prisma({
+  typeDefs: "src/generated/prisma.graphql",
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;
