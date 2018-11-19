b4efac11297366bbefc4c5cb5469d9ca424799de
Matt Nierzwicki <matt.nierzwicki@gmail.com>
2018-11-19 14:17:10 -0500

    Implement pagination for shop items

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -5,10 +5,11 @@ import styled from "styled-components";
 
 import Item from "./Item";
 import Pagination from "./Pagination";
+import { perPage } from "../config";
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -36,7 +37,12 @@ class Items extends React.Component {
     return (
       <Center>
         <Pagination page={this.props.page} />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{
+            skip: this.props.page * perPage - perPage
+          }}
+        >
           {payload => {
             if (payload.loading) return <p>Loading...</p>;
             if (payload.error) return <p>Error: {payload.error.message}</p>;
