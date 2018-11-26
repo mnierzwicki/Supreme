222a0c5ae192fb13eaf7ea9c3eab7d387713be73
Matt Nierzwicki <matt.nierzwicki@gmail.com>
2018-11-25 20:43:37 -0500

    Display cart count component next to user's cart

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,7 +4,8 @@ import { Mutation } from "react-apollo";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
 import SignOut from "./SignOut";
-import { TOGGLE_CART_MUTATION } from "../components/Cart";
+import { TOGGLE_CART_MUTATION } from "./Cart";
+import CartCount from "./CartCount";
 
 const Nav = () => (
   <User>
@@ -30,7 +31,13 @@ const Nav = () => (
                 <a>Account</a>
               </Link>
               <SignOut />
-              <Mutation mutation={TOGGLE_CART_MUTATION}>{toggleCart => <button onClick={toggleCart}>🛒</button>}</Mutation>
+              <Mutation mutation={TOGGLE_CART_MUTATION}>
+                {toggleCart => (
+                  <button onClick={toggleCart}>
+                    🛒 <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)} />
+                  </button>
+                )}
+              </Mutation>
             </>
           )}
 
