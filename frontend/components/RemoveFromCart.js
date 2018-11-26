efe5df23eb805888645c471c3425ee0f2e4ee64b
Matt Nierzwicki <matt.nierzwicki@gmail.com>
2018-11-25 20:07:34 -0500

    Optimistically update cart state after removing an item

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -28,9 +28,36 @@ class RemoveFromCart extends React.Component {
     id: PropTypes.string.isRequired
   };
 
+  // Method called twice on item removal:
+  // First call is immediately after a user attemps to
+  // remove an item via optimisticReponse.
+  // Second call is after we receive the server response
+  update = (cache, payload) => {
+    // Read the cache
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+
+    // Remove item from the cart
+    const removedCartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== removedCartItemId);
+
+    // Write the deletion back to the cache
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data: data });
+  };
+
   render() {
     return (
-      <Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={{ id: this.props.id }}>
+      <Mutation
+        mutation={REMOVE_FROM_CART_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+        optimisticResponse={{
+          __typename: "Mutation",
+          removeFromCart: {
+            __typename: "CartItem",
+            id: this.props.id
+          }
+        }}
+      >
         {(removeFromCart, { loading, error }) => (
           <Button
             disabled={loading}
