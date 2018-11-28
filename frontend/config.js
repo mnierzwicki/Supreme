b7572e92f05fc9c0e96299692909f78c0a940b1f
Matt Nierzwicki <matt.nierzwicki@gmail.com>
2018-11-28 13:19:10 -0500

    Add backend prod endpoint to config

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
+export const prodEndpoint = `https://supreme-store-backend-prod.now.sh`;
 export const perPage = 4;
 export const STRIPE_PUB_KEY = "pk_test_znfXSh0CN8B0n7GqXJ1Rs0zZ";
